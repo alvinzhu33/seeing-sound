ad0a0142db56e6bec544675df85c283a5cc902bd
Julia <fiksin@mit.edu>
2018-04-06 04:42:34 -0400

    added basic animations

---
+++ Scripts/Test.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+    float yaxis;
+
+	// Use this for initialization
+	void Start () {
+
+      
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        yaxis = transform.position.y;
+
+
+        //AudioManager.singleton.PlayEvent(AudioManager.play_example, gameObject);
+
+        //AudioManager.singleton.SetRTPC(AudioManager.rtpc_example, yaxis);
+
+
+
+
+
+    }
+}
